b466227136e92a10b531d2defaf43e058f512fc7
Masa Orel <orel.masha89@gmail.com>
2023-01-04 11:13:13 +0100

    added changes to projects

--- script.js
+++ script.js
@@ -42,3 +42,19 @@ function copyToClipboard() {
 
 const emailCopy = document.querySelector("#email");
 
+//slide in on scroll
+const box = document.querySelector(".left")
+
+window.addEventListener("scroll", checkBoxes)
+// where is the trigger point
+function checkBoxes(){
+  const fadeIn = window.innerHeight / 5 * 3
+
+  const boxTop = box.getBoundingClientRect().top
+  if(boxTop < fadeIn){
+    box.classList.add("show")
+  } else {
+    box.classList.remove("show")
+  }
+}
+
